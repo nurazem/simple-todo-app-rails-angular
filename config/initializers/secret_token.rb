e41bf7250cb4e3d293ea2ac06b5a7275b2d5af4c
Nurazem Kaldybaev <nurazemkg@gmail.com>
2013-12-22 01:22:37 +0400

    Initial Commit: Created an new Rails project with no test files included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoApp::Application.config.secret_key_base = 'fa513c097739ba07e335b0f116e76efd9bfa86e907c0f9468708b5d9e56263dd9597f761e3b6e8f5b008603ebc2449bead5f6d274842ec27d3d173ea26222d09'
